644032175b5c491b1c59f7e961c40887983b83dc
Frederik Göbel <frederikgoebel@gmx.de>
2020-10-08 11:20:17 +0200

    Still messy but way better structure. Also moved animations to gsap.

--- code.js
+++ code.js
@@ -43,7 +43,6 @@ async function setup() {
   const ctx = canvas.getContext('2d');
   const videoContext = videoBuffer.getContext('2d');
   let lineWidth = 1;
-  let imageSnapped = false;
   const scale = 3.3;
   const h = 200;
   var circle = new SAT.Circle(new SAT.Vector(videoWidth / 2, h - 30 * scale), 30 * scale);
@@ -62,105 +61,233 @@ async function setup() {
     const flipHorizontal = true;
     const outputStride = 16;
 
-    async function oneFrame() {
+    async function humanInShape() {
       const pose = await net.estimateSinglePose(video, {
         flipHorizontal: true,
         decodingMethod: 'single-person'
       });
 
-      ctx.clearRect(0, 0, videoWidth, videoHeight);
-
-      if (!imageSnapped) {
-        videoContext.save();
-        videoContext.scale(-1, 1);
-        videoContext.translate(-videoWidth, 0);
-        videoContext.drawImage(video, 0, 0, videoWidth, videoHeight);
-        videoContext.restore();
-      }
-
-      ctx.save();
-      ctx.drawImage(videoBuffer, 0, 0, videoWidth, videoHeight);
-      ctx.restore();
-      let allIn = true;
-
-      drawKeypoints(pose.keypoints, 0.6, ctx);
-
       pose.keypoints = pose.keypoints.filter(({score}) => score > 0.6);
       if (pose.keypoints.length == 0)
         allIn = false;
+
+      let allIn = true;
       pose.keypoints.forEach(({position}) => {
         var v = new SAT.Vector(position.x, position.y);
         allIn &= SAT.pointInPolygon(v, body) || SAT.pointInCircle(v, circle);
       })
-
-
-      let shapeColor;
-      if (allIn) {
-        shapeColor = "transparent";
-        lineWidth = Math.ceil(lineWidth * 1.3);
-      } else {
-        shapeColor = "rgba(0,255,0,0.5)";
-        lineWidth = 5;
-      }
-
-
-      if (lineWidth < 100) {
-        drawPoint(ctx, shapeColor);
-
-        ctx.beginPath();
-        ctx.arc(circle.pos.x, circle.pos.y, circle.r, 0, 2 * Math.PI);
-        ctx.save();
-        ctx.clip();
-        ctx.strokeStyle = "rgba(0,255,0,0.5)"
-        //ctx.fill();
-        ctx.stroke();
-        ctx.restore();
-
-        ctx.save(),
-        ctx.fillStyle = shapeColor;
-        ctx.strokeStyle = "rgba(0,255,0,0.5)"
-        ctx.beginPath();
-        ctx.translate(body.pos.x, body.pos.y);
-        ctx.moveTo(0, 0);
-        body.points.forEach((vector) => {
-          ctx.lineTo(vector.x, vector.y);
-        });
-        ctx.closePath();
-        ctx.save();
-        ctx.clip();
-        ctx.lineWidth = lineWidth;
-        //ctx.fill();
-        ctx.stroke();
-        ctx.restore();
-        ctx.restore();
-      } else if (!imageSnapped) {
-        ctx.fillStyle = "white";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        imageSnapped = true;
-      } else {
-      }
-
-      requestAnimationFrame(oneFrame)
+      // drawKeypoints(pose.keypoints, 0.6, ctx)
+      return allIn
     }
-    oneFrame()
+
+    function saveVideoToBuffer(video, buffer) {
+      buffer.save();
+      buffer.scale(-1, 1);
+      buffer.translate(-videoWidth, 0);
+      buffer.drawImage(video, 0, 0, videoWidth, videoHeight);
+      buffer.restore();
+    }
+
+
+    function drawBody(color) {
+      ctx.fillStyle = ctx.strokeStyle = color;
+
+      // head
+      ctx.beginPath();
+      ctx.arc(circle.pos.x, circle.pos.y, circle.r, 0, 2 * Math.PI);
+
+      // // body
+      ctx.save();
+      ctx.translate(body.pos.x, body.pos.y);
+      ctx.moveTo(0, 0);
+      body.points.forEach((vector) => {
+        ctx.lineTo(vector.x, vector.y);
+      });
+      ctx.closePath();
+      ctx.save();
+      ctx.clip();
+      ctx.lineWidth = bodyObj.lineWidth;
+      ctx.stroke();
+      ctx.restore();
+      ctx.restore();
+    }
+
+    async function idle() {
+
+      let allIn = await humanInShape()
+      ctx.clearRect(0, 0, videoWidth, videoHeight);
+
+      saveVideoToBuffer(video, videoContext)
+
+      // bg
+      ctx.save();
+      ctx.drawImage(videoBuffer, 0, 0, videoWidth, videoHeight);
+      ctx.restore();
+
+      drawBody("rgba(255,0,0,0.5)");
+
+      if (allIn)
+        return "found"
+      return "idle"
+    }
+
+
+    let bodyObj = {
+      lineWidth: 5,
+    }
+
+    var fadeTl = gsap.timeline({});
+    fadeTl.pause();
+    fadeTl.to(bodyObj, {
+      duration: 3,
+      lineWidth: 350,
+      ease: "sine.in",
+    });
+
+    async function found() {
+      let allIn = await humanInShape()
+      ctx.clearRect(0, 0, videoWidth, videoHeight);
+
+      saveVideoToBuffer(video, videoContext)
+
+      ctx.save();
+      ctx.drawImage(videoBuffer, 0, 0, videoWidth, videoHeight);
+      ctx.restore();
+
+      drawBody("rgba(0,255,0,0.5)");
+
+      if (fadeTl.totalProgress() == 1)
+        return "flash"
+      else if (!allIn)
+        return "idle"
+      return "found"
+    }
+
+    function idle2found() {
+      fadeTl.play();
+    }
+
+    function found2idle() {
+      fadeTl.reverse();
+    }
+
+
+    let flashObj = {
+      lumen: 1.0,
+    }
+    var flashTl = gsap.timeline({});
+    flashTl.pause()
+    flashTl.to(flashObj, {
+      lumen: 0.0,
+      duration: 0.7,
+      ease: "power4.inOut",
+    })
+
+
+    function found2flash() {
+      flashTl.play();
+      saveVideoToBuffer(video, videoContext);
+    }
+
+    async function flash() {
+      ctx.clearRect(0, 0, videoWidth, videoHeight);
+
+      ctx.save();
+      ctx.drawImage(videoBuffer, 0, 0, videoWidth, videoHeight);
+      ctx.restore();
+
+      ctx.rect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = "rgba(255,255,255," + flashObj.lumen + ")";
+      ctx.fill();
+
+
+      if (flashTl.totalProgress() == 1)
+        return "colorSteal"
+      return "flash"
+    }
+
+    async function colorSteal() {
+      ctx.clearRect(0, 0, videoWidth, videoHeight);
+      ctx.save();
+      ctx.drawImage(videoBuffer, 0, 0, videoWidth, videoHeight);
+      ctx.restore();
+      return "colorSteal"
+    }
+
+    async function lost() {
+      let allIn = await humanInShape()
+      ctx.clearRect(0, 0, videoWidth, videoHeight);
+
+      videoContext.save();
+      videoContext.scale(-1, 1);
+      videoContext.translate(-videoWidth, 0);
+      videoContext.drawImage(video, 0, 0, videoWidth, videoHeight);
+      videoContext.restore();
+
+      ctx.save();
+      ctx.drawImage(videoBuffer, 0, 0, videoWidth, videoHeight);
+      ctx.restore();
+
+      ctx.save(),
+      ctx.fillStyle = "rgba(0,255,0,0.5)";
+      ctx.strokeStyle = "rgba(0,255,0,0.5)"
+      ctx.beginPath();
+      ctx.translate(body.pos.x, body.pos.y);
+      ctx.moveTo(0, 0);
+      body.points.forEach((vector) => {
+        ctx.lineTo(vector.x, vector.y);
+      });
+      ctx.closePath();
+      ctx.save();
+      ctx.clip();
+      ctx.lineWidth = lineWidth;
+      //ctx.fill();
+      ctx.stroke();
+      ctx.restore();
+      ctx.restore();
+
+      lineWidth /= 1.5;
+
+      if (lineWidth <= 5)
+        return "idle"
+      else if (allIn)
+        return "found"
+      return "lost"
+    }
+
+    let states = {
+      "idle": idle,
+      "found": found,
+      "lost": lost,
+      "flash": flash,
+      "colorSteal": colorSteal,
+    }
+    let transitions = {
+      "idle": {
+        "found": idle2found
+      },
+      "found": {
+        "idle": found2idle,
+        "flash": found2flash,
+      },
+      "lost": {},
+      "flash": {},
+      "colorSteal": {},
+    }
+    let state = "idle"
+    async function oneFrame() {
+      let newState = await states[state]();
+      if (transitions[state][newState]) {
+        transitions[state][newState]();
+      }
+      state = newState;
+      requestAnimationFrame(oneFrame);
+    }
+    oneFrame();
 
 
   });
 
 }
-
-
-// function main(){
-//   switch(state){
-//     case Idle:
-//       recognize position
-//       case position recognized:
-//         fill shape;
-//       case shape full:
-//       snap image;
-//       case image there:
-//         other people deal with dis;
-//   }
-// }
 
 setup();
